e42fcd0f790f950e64ac7692a812af079683cf12
mindtechnist <2353924328@qq.com>
2026-06-29 07:17:23 +0800

    1

---
+++ .history/front-end/javascript/lesson-4/closure_20260629003452.js
@@ -0,0 +1,19 @@
+//闭包的作用
+//1.数据私有化
+function closureFn(){
+  let count = 0
+  return {
+    getValue: ()=> count
+    setValue: (val)=>count = val
+  }
+}
+
+
+var counter = closureFn()
+
+console.log(counter.getValue());
+console.log(counter.setValue(123));
+
+
+//课后作业：同时拿到值并操作它
+// 课后作业： 拿到这个值/操作值
